249b3980bf05dec9a26f4ca382b74a577f3666d2
kayxean <36327242+kayxean@users.noreply.github.com>
2025-11-19 22:11:51 +0700

    feat: add more useful aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,14 +1,18 @@
-alias now="date +'%I:%M %p, %a, %b %d'"
+alias z="__zoxide_z"
+alias zi="__zoxide_zi"
+
+alias list="eza --across --group-directories-first --classify=auto"
+alias ls="list"
+alias la="list --all"
+alias ll="list --all --long --git --git-repos-no-status --time-style='relative' --total-size"
+alias lr="list --recurse"
+alias lt="list --recurse --tree"
 
 alias view="bat --style=plain"
 alias help="bat --language=help --style=plain"
 
-alias chrome="google-chrome-stable"
+alias now="date +'%I:%M %p, %a, %b %d'"
+alias exit="hyprctl dispatch exit"
 
-alias z="__zoxide_z"
-alias zi="__zoxide_zi"
-
-alias ll="eza --across --group-directories-first --classify=auto --recurse"
-alias li="eza --across --group-directories-first --classify=auto --recurse --tree"
-alias ls="eza --all --across --group-directories-first --classify=auto"
-alias la="eza --all --across --group-directories-first --classify=auto --long --git --git-repos-no-status --time-style='relative' --total-size"
+alias firewall="sudo ufw status verbose"
+alias service="systemctl list-units --type=service --all --no-legend --plain"
